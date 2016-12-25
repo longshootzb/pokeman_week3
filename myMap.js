2b21415c88a6e0cd8a6679bd987f6937a8436f68
longshootzb <longshootzb@users.noreply.github.com>
2016-12-25 21:35:52 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,7 @@
-                var map;
-                function loadMapScenario() {
-                    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-                        credentials: 'Aun7JsCI6Ez6bKsgtzihWsj5tiEUrIcHCofcV0NznlxRxK5lzGUE-0z-MDlnE_Nu'
-                    });
-                }
+
+var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+    credentials: 'Aun7JsCI6Ez6bKsgtzihWsj5tiEUrIcHCofcV0NznlxRxK5lzGUE-0z-MDlnE_Nu'
+});
+var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://raw.githubusercontent.com/chenditc/mypokemon.io/gh-pages/images/pushpin_images/pokemon/1.png',
+    anchor: new Microsoft.Maps.Point(12, 39) });
+map.entities.push(pushpin);
