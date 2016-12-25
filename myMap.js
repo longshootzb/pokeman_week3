203264eab5e3a3323fe2ee408c770641e7ff2b8b
longshootzb <longshootzb@users.noreply.github.com>
2016-12-25 21:23:34 +0800

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,6 @@
+                var map;
+                function loadMapScenario() {
+                    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+                        credentials: 'Aun7JsCI6Ez6bKsgtzihWsj5tiEUrIcHCofcV0NznlxRxK5lzGUE-0z-MDlnE_Nu'
+                    });
+                }
