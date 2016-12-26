c66f70f23aad0399e4d9141193993fe400f77556
longshootzb <longshootzb@users.noreply.github.com>
2016-12-26 12:22:32 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,7 +1,2 @@
 
-var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-    credentials: 'Aun7JsCI6Ez6bKsgtzihWsj5tiEUrIcHCofcV0NznlxRxK5lzGUE-0z-MDlnE_Nu'
-});
-var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://raw.githubusercontent.com/chenditc/mypokemon.io/gh-pages/images/pushpin_images/pokemon/1.png',
-    anchor: new Microsoft.Maps.Point(12, 39) });
-map.entities.push(pushpin);
+
