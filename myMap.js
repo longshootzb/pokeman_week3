0e74bd842c76bc4b3b7de98e73373750c088d383
longshootzb <longshootzb@users.noreply.github.com>
2016-12-26 23:43:46 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
   function loadMapScenario() {
                 var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-                    credentials: 'Your Bing Maps Key'
+                    credentials: 'Aun7JsCI6Ez6bKsgtzihWsj5tiEUrIcHCofcV0NznlxRxK5lzGUE-0z-MDlnE_Nu'
                 });
                 var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
                     anchor: new Microsoft.Maps.Point(12, 39) });
