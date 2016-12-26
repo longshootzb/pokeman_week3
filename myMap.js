1354afbea64888b77bb0d6a8a4c0dfa665193f81
longshootzb <longshootzb@users.noreply.github.com>
2016-12-27 06:39:24 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -2,7 +2,7 @@
                 var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
                     credentials: 'Aun7JsCI6Ez6bKsgtzihWsj5tiEUrIcHCofcV0NznlxRxK5lzGUE-0z-MDlnE_Nu'
                 });
-                var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+                var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
                     anchor: new Microsoft.Maps.Point(12, 39) });
                 map.entities.push(pushpin);
                 
