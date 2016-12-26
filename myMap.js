58487cb66cc030e7129698bac0c7ef222abeadb6
longshootzb <longshootzb@users.noreply.github.com>
2016-12-27 06:49:41 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -3,7 +3,7 @@
                     credentials: 'Aun7JsCI6Ez6bKsgtzihWsj5tiEUrIcHCofcV0NznlxRxK5lzGUE-0z-MDlnE_Nu'
                 });
                 var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
-                    anchor: new Microsoft.Maps.Point(12, 39) });
+                     });
                 map.entities.push(pushpin);
                 
             }
