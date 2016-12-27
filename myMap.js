92c6be84edac0ceefa3d9a333d517566afb0d739
longshootzb <longshootzb@users.noreply.github.com>
2016-12-27 13:01:32 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -2,7 +2,7 @@
                 var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
                     credentials: 'Your Bing Maps Key'
                 });
-                var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+                var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/longshootzb/pokeman_week3/blob/master/pokemon/1.png',
                     anchor: new Microsoft.Maps.Point(12, 39) });
                 map.entities.push(pushpin);
                 
