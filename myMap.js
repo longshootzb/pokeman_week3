2a580ea05b107017afac3982ae02f610e679303b
longshootzb <longshootzb@users.noreply.github.com>
2016-12-27 12:44:50 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,9 +1,9 @@
-function loadMapScenario() {
-           var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-               credentials: 'Aun7JsCI6Ez6bKsgtzihWsj5tiEUrIcHCofcV0NznlxRxK5lzGUE-0z-MDlnE_Nu'
-           });
-           var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj21v6Vx5PRAhUD92MKHa0WCzEQjRwIBw&url=http%3A%2F%2Fpic.hao123.com%2Faimg%2Flianglimeizi%2F7746571%3Fact%3D&psig=AFQjCNGNQGSJJE6tVKTCGsevwbBM78nwqg&ust=1482900003653146',
+            function loadMapScenario() {
+                var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+                    credentials: 'Your Bing Maps Key'
                 });
-           map.entities.push(pushpin);
-
-       }
+                var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+                    anchor: new Microsoft.Maps.Point(12, 39) });
+                map.entities.push(pushpin);
+                
+            }
